3575db91731f8efbd7e53b959198f9be38e55b68
Grant George <grant@grantgeorge.io>
2018-03-14 20:32:16 -0700

    test

--- src/HeroImage/HeroImageWrapper.js
+++ src/HeroImage/HeroImageWrapper.js
@@ -3,13 +3,9 @@
 import styled from 'styled-components'
 
 const HeroImageWrapper = styled.div`
-  margin: 0 -15px 1rem;
+  margin: 0;
   box-shadow: 0 3px 6px 1px rgba(0, 0, 0, 0.16),
     0 3px 6px 1px rgba(0, 0, 0, 0.23);
-
-  @media (min-width: 768px) {
-    margin: 0;
-  }
 `
 
 export default HeroImageWrapper
